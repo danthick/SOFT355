0b94242b451ba7f4d5874c9ecd2f047e89959c5b
Dan Thick <dan.thick@hotmail.co.uk>
2019-12-08 14:45:24 +0000

    Build test

--- db_user.js
+++ db_user.js
@@ -3,7 +3,6 @@ var schemas = require("./schemas");
 
 const uri = "mongodb+srv://dthick:kznvRYL5QeIHMNdL@soft355-vjwy9.mongodb.net/test?retryWrites=true&w=majority";
 
-
 MongoClient.connect(uri, function(err, db) {
     var user = new schemas.User({"email": "dan.thick@hotmail.co.uk", "password": "password", "firstName": "Dan", "lastName": "Thick"});
     db.db("db").collection("users").insertOne(user);

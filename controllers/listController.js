37b4c0b43ab5fa930e1d48b009b0954574a282cf
Dan Thick <dan.thick@hotmail.co.uk>
2019-12-28 15:39:56 +0000

    Adding database functions

--- controllers/listController.js
+++ controllers/listController.js
@@ -4,7 +4,7 @@ module.exports = function(app){
     app.get("/todo", function(request, response){
         console.log("Viewing the todo list");
         response.sendFile(path.join(__dirname , '../views/', 'list.html'));
-        //response.sendFile('./views/list.html');
+
     });
 
 
--- db_user.js
+++ db_user.js
@@ -1,10 +1,26 @@
 const MongoClient = require('mongodb').MongoClient;
 var schemas = require("./schemas");
-
 const uri = "mongodb+srv://dthick:kznvRYL5QeIHMNdL@soft355-vjwy9.mongodb.net/test?retryWrites=true&w=majority";
 
+// POST data
 MongoClient.connect(uri, function(err, db) {
+    var users = db.db("db").collection("users");
     var user = new schemas.User({"email": "dan.thick@hotmail.co.uk", "password": "password", "firstName": "Dan", "lastName": "Thick"});
-    db.db("db").collection("users").insertOne(user);
+    var user2 = new schemas.User({"email": "amyjagla@gmail.com", "password": "password", "firstName": "Amy", "lastName": "Jagla"});
+    var user3 = new schemas.User({"email": "aim-me@gmail.com", "password": "password", "firstName": "Amy", "lastName": "Jagla"});
+    
+    //users.insertOne(user);
+    //users.insertOne(user2);
+    //users.insertOne(user3);
+
+    // GET data
+    users.find({firstName: "Amy"}).toArray().then((data) => {
+
+    });
+
+    // DELETE data
+    users.deleteOne({email: "aim-me@gmail.com"});
+
     db.close();
 });
+
